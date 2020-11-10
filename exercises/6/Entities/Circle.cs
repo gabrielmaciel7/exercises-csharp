0a7c17810bb887199a80f27101efcf157e740b28
gabriel.soares02 <gabriel.soares02@latam.com>
2020-11-10 14:46:33 -0300

    add 6

---
+++ exercises/6/Entities/Circle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _6.Entities
+{
+    class Circle
+    {
+        private readonly double _pi = Math.PI;
+
+        public float Radius { get; set; }
+
+        public Circle(float radius)
+        {
+            Radius = radius;
+        }
+
+        public double Area()
+        {
+            return _pi * Radius * Radius;
+        }
+    }
+}
---
+++ exercises/6/Program.cs
@@ -0,0 +1,19 @@
+﻿using _6.Entities;
+using System;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o raio do círculo: ");
+            var radius = float.Parse(Console.ReadLine());
+
+            var circle = new Circle(radius);
+            var circleArea = circle.Area();
+
+            Console.WriteLine($"Área: {circleArea}");
+        }
+    }
+}
