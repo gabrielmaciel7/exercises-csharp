f66839db49c04e9e5bd1e0de1436be446fc51576
gabriel.soares02 <gabriel.soares02@latam.com>
2020-11-10 16:45:28 -0300

    add 10

---
+++ exercises/10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor da temperatura em graus Celsius: ");
+            var degreesC = float.Parse(Console.ReadLine());
+
+            var degreesF = CelsiusToFahrenheit(degreesC);
+
+            Console.WriteLine($"Temperatura em graus Fahrenheit: {degreesF}°F");
+        }
+
+        static float CelsiusToFahrenheit(float degreesC)
+        {
+            return (degreesC * 9 / 5) + 32;
+        }
+    }
+}
