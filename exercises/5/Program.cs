e94b7702fd96f711651680bcb4418c20791d2074
gabriel.soares02 <gabriel.soares02@latam.com>
2020-11-10 14:23:24 -0300

    add 5

---
+++ exercises/5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um valor em metros: ");
+            var meters = float.Parse(Console.ReadLine());
+
+            var centimeters = MetersToCentimeters(meters);
+
+            Console.WriteLine($"Centimeters: {centimeters}");
+        }
+
+        static float MetersToCentimeters(float meters)
+        {
+            return meters * 100;
+        }
+    }
+}
