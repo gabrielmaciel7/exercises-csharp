99c1fa6b8a61ca278e977b4fb145feb90bc89261
gabriel.soares02 <gabriel.soares02@latam.com>
2020-11-10 13:46:36 -0300

    add 1 and 2

---
+++ exercises/2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+
+            var number = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"O número informado foi {number}");
+        }
+    }
+}
