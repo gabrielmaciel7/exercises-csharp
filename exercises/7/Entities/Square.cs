f5b0816a13321e1abe3e6187e87e0f348acd82b2
gabriel.soares02 <gabriel.soares02@latam.com>
2020-11-10 14:57:39 -0300

    add 7

---
+++ exercises/7/Entities/Square.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _7.Entities
+{
+    class Square
+    {
+        public float Side { get; set; }
+
+        public Square(float side)
+        {
+            Side = side;
+        }
+
+        public float Area()
+        {
+            return Side * Side;
+        }
+    }
+}
---
+++ exercises/7/Program.cs
@@ -0,0 +1,19 @@
+﻿using _7.Entities;
+using System;
+
+namespace _7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o lado do quadrado: ");
+            var side = float.Parse(Console.ReadLine());
+
+            var square = new Square(side);
+            var squareArea = square.Area();
+
+            Console.WriteLine($"Dobro da área: {squareArea * 2}");
+        }
+    }
+}
