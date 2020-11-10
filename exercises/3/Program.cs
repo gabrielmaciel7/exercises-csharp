a841464db7437138a6e703a69d861be21ecd2310
gabriel.soares02 <gabriel.soares02@latam.com>
2020-11-10 13:54:33 -0300

    add 3

---
+++ exercises/3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o primeiro número: ");
+            var number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            var number2 = int.Parse(Console.ReadLine());
+
+            var soma = Soma(number1, number2);
+
+            Console.WriteLine($"Soma: {soma}");
+        }
+
+        static int Soma(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+    }
+}
