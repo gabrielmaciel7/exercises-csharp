66fb87a3bc05a1b7dc76ac4b5b7751d5319fddb9
gabriel.soares02 <gabriel.soares02@latam.com>
2020-11-10 16:34:49 -0300

    add 8

---
+++ exercises/8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o valor da hora trabalhada: ");
+            var hrValue = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite a quantidade de horas trabalhadas no mês: ");
+            var hrsWorked = int.Parse(Console.ReadLine());
+
+            var salary = Salary(hrValue, hrsWorked);
+
+            Console.WriteLine($"Salário no mês: {salary}");
+        }
+
+        static float Salary(float hrValue, int hrsWorked)
+        {
+            return hrValue * hrsWorked;
+        }
+    }
+}
