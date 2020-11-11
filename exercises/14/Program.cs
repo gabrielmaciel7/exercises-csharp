69031cd0d573c4dcc4b53991678109766d2ec309
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-11 15:22:49 -0300

    add 14

---
+++ exercises/14/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o peso de peixes (kg): ");
+            var weight = float.Parse(Console.ReadLine());
+
+            var excess = CalculateExcess(weight);
+            var fine = CalculateFine(excess);
+
+            Console.WriteLine($"Excesso: {excess}");
+            Console.WriteLine($"Multa: {fine}");
+        }
+
+        static float CalculateExcess(float weight)
+        {
+            var limit = 50;
+
+            if (weight < limit)
+            {
+                return 0;
+            }
+
+            return weight - limit;
+        }
+
+        static float CalculateFine(float excess)
+        {
+            float finePerKg = 4;
+
+            return excess * finePerKg;
+        }
+    }
+}
