83d770ec6579a8651e907c394e744632e451573b
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-11 15:03:11 -0300

    add 13

---
+++ exercises/13/Entities/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _13.Entities
+{
+    class Person
+    {
+        public float Height { get; set; }
+
+        public Person(float height)
+        {
+            Height = height;
+        }
+
+        public float IdealWeightForMen()
+        {
+            return ((float)72.7 * Height) - 58;
+        }
+
+        public float IdealWeightForWomen()
+        {
+            return ((float)62.1 * Height) - (float)44.7;
+        }
+    }
+}
---
+++ exercises/13/Program.cs
@@ -0,0 +1,21 @@
+﻿using _13.Entities;
+using System;
+
+namespace _13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a altura (h) de uma pessoa: ");
+            var height = float.Parse(Console.ReadLine());
+
+            var person = new Person(height);
+            var idealWeightForMen = person.IdealWeightForMen();
+            var idealWeightForWomen = person.IdealWeightForWomen();
+
+            Console.WriteLine($"Peso ideal para homens: {idealWeightForMen}");
+            Console.WriteLine($"Peso ideal para mulheres: {idealWeightForWomen}");
+        }
+    }
+}
