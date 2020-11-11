91627af1a6c70cb3b1737916e57ca54ffd93430c
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-11 14:57:42 -0300

    add 12

---
+++ exercises/12/Entities/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _12.Entities
+{
+    class Person
+    {
+        public float Height { get; set; }
+
+        public Person(float height)
+        {
+            Height = height;
+        }
+
+        public float IdealWeight()
+        {
+            return ((float)72.7 * Height) - 58;
+        }
+    }
+}
---
+++ exercises/12/Program.cs
@@ -0,0 +1,19 @@
+﻿using _12.Entities;
+using System;
+
+namespace _12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a altura (h) de uma pessoa: ");
+            var height = float.Parse(Console.ReadLine());
+
+            var person = new Person(height);
+            var idealWeight = person.IdealWeight();
+
+            Console.WriteLine($"Peso ideal: {idealWeight}");
+        }
+    }
+}
