f2166fdaad72409b7dd37f65b3a0ced17af05e5f
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-12 10:31:19 -0300

    add 16

---
+++ exercises/16/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _16
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o tamanho em metros quadrados da área a ser pintada: ");
+            var area = float.Parse(Console.ReadLine());
+
+            var paintCans = CalculateAmountOfPaintCans(area);
+            var price = CalculatePrice(paintCans);
+
+            Console.WriteLine($"Quantidade de latas de tinta a serem compradas: {paintCans}");
+            Console.WriteLine($"Preço total: R${price:F2}");
+        }
+
+        static int CalculateAmountOfPaintCans(float area)
+        {
+            var paintRate = (1F / 3);
+            var canSize = 18;
+
+            var litersRequired = paintRate * area;
+            var cansRequired = (int)Math.Ceiling(litersRequired / canSize);
+
+            return cansRequired;
+        }
+
+        static float CalculatePrice(int paintCans)
+        {
+            var unitPrice = 80F;
+
+            return unitPrice * paintCans;
+        }
+    }
+}
