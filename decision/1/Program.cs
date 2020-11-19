ee877b8dc8d51b73cea7305d9db8160c95105d7c
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-19 10:10:09 -0300

    add decision 1

---
+++ decision/1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número: ");
+            var number1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite outro número: ");
+            var number2 = float.Parse(Console.ReadLine());
+
+            var numbers = new List<float> { number1, number2 };
+
+            Console.WriteLine($"Maior: {numbers.Max()}");
+        }
+    }
+}
