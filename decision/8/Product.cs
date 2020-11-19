d00b8dd2c4a855ad5d3f5380668049af92574d02
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-19 11:52:20 -0300

    add decision 8

---
+++ decision/8/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _8
+{
+    class Product
+    {
+        public string Name { get; set; }
+        public float Price { get; set; }
+
+        public Product(string name, float price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ decision/8/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o preço do produto 1: ");
+            var price1 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o preço do produto 2: ");
+            var price2 = float.Parse(Console.ReadLine());
+
+            Console.Write("Digite o preço do produto 3: ");
+            var price3 = float.Parse(Console.ReadLine());
+
+            var products = new List<Product>
+            {
+                new Product("Produto 1", price1),
+                new Product("Produto 2", price2),
+                new Product("Produto 3", price3)
+            };
+
+            var minProducts = products.Where(product => product.Price == products.Min(product => product.Price));
+
+            foreach (var minProduct in minProducts)
+            {
+                Console.WriteLine($"Deve comprar: {minProduct.Name}");
+            }
+        }
+
+
+    }
+}
