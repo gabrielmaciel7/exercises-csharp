d7ebb2820f2ac4df968e863ffb3476814225cced
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-19 14:01:20 -0300

    add decision 10

---
+++ decision/10/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Em que turno você estuda? (M-matutino ou V-Vespertino ou N-Noturno) ");
+            var shift = Console.ReadLine();
+
+            var output = (shift.ToUpper()) switch
+            {
+                "M" => "Bom Dia!",
+                "V" => "Boa Tarde!",
+                "N" => "Boa Noite!",
+                _ => "Valor Inválido!"
+            };
+
+            Console.WriteLine(output);
+        }
+    }
+}
---
+++ decision/9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite o número 1: ");
+            var number1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o número 2: ");
+            var number2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o número 3: ");
+            var number3 = int.Parse(Console.ReadLine());
+
+            var numbers = new List<int> { number1, number2, number3 };
+
+            Console.WriteLine("Decrescente:");
+
+            foreach (var number in numbers.OrderByDescending(number => number))
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
