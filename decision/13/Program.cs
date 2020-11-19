a2a1e10e0efd2dd4efe442f8d2e6535c6fc275a8
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-19 14:46:21 -0300

    add decision 13

---
+++ decision/13/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um numero: ");
+            var number = int.Parse(Console.ReadLine());
+
+            var weekDay = number switch
+            {
+                1 => "1 - Domingo",
+                2 => "2 - Segunda",
+                3 => "3 - Terça",
+                4 => "4 - Quarta",
+                5 => "5 - Quinta",
+                6 => "6 - Sexta",
+                7 => "7 - Sábado",
+                _ => "Valor inválido"
+            };
+
+            Console.WriteLine($"Dia da semana: {weekDay}");
+        }
+    }
+}
