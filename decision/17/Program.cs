b633c7ac8e1df93f590eaca154fffcd59caa8476
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-20 10:37:36 -0300

    add decision 17

---
+++ decision/17/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um ano: ");
+            var year = int.Parse(Console.ReadLine());
+
+            var isLeap = IsLeapYear(year);
+
+            Console.WriteLine(isLeap ? "Ano bissexto." : "Não é bissexto.");
+        }
+
+        static bool IsLeapYear(int year)
+        {
+            if (year % 4 != 0)
+            {
+                return false;
+            }
+
+            if (year % 100 == 0)
+            {
+                if (year % 400 != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
