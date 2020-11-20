4fc9bf534ed9a86a517df27e2911c06d8821101f
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-20 10:44:00 -0300

    add decision 18

---
+++ decision/18/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite uma data (dd/mm/aaaa): ");
+
+            try
+            {
+                var date = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine($"Data válida: {date:dd/MM/yyyy}");
+            }
+            catch
+            {
+                Console.WriteLine("Data inválida.");
+            }
+        }
+    }
+}
