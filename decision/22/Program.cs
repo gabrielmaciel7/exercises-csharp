d6116e416d1063fc7b94b1000ce6ad7dd3b7c1d1
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-23 10:19:08 -0300

    add decision 22

---
+++ decision/22/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro: ");
+            var number = int.Parse(Console.ReadLine());
+
+            var isEven = number % 2 == 0;
+
+            Console.WriteLine(isEven ? "Par" : "Ímpar");
+        }
+    }
+}
