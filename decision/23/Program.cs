b7aab95d2daf7d6739c6224b4a8cd6236674e526
gabrielmaciel7 <gabrielmaciel.nave@gmail.com>
2020-11-23 10:23:57 -0300

    add decision 23

---
+++ decision/23/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite um número inteiro: ");
+            var number = double.Parse(Console.ReadLine());
+
+            var isDecimal = Math.Round(number) != number;
+
+            Console.WriteLine(isDecimal ? "Decimal" : "Inteiro");
+        }
+    }
+}
